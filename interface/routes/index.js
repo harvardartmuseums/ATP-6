e3000fcdc56eebe1bc108d6aab38314c3e884f78
Jeff Steward <steward.jeff@gmail.com>
2023-09-20 18:13:22 -0400

    fine tune the homepage

--- interface/routes/index.js
+++ interface/routes/index.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'The Trailhead | Art Forest | ATP 6 | Harvard Art Museums' });
+  res.render('index', { title: 'The Trailhead | Art Forest | MAN-17 | Harvard Art Museums' });
 });
 
 router.get('/controller', function(req, res, next) {
-  res.render('controller', { title: 'The Controller | Art Forest | ATP 6 | Harvard Art Museums' });
+  res.render('controller', { title: 'The Controller | Art Forest | MAN-17| Harvard Art Museums' });
 });
 
 router.get('/forest', function(req, res, next) {
-  res.render('forest', { title: 'The Forest | Art Forest | ATP 6 | Harvard Art Museums' });
+  res.render('forest', { title: 'The Forest | Art Forest | MAN-17| Harvard Art Museums' });
 });
 
 router.get('/history', function(req, res, next) {
-  res.render('history', { title: 'The History | Art Forest | ATP 6 | Harvard Art Museums' });
+  res.render('history', { title: 'The History | Art Forest | MAN-17| Harvard Art Museums' });
 });
 
 router.get('/mcp', function(req, res, next) {
-  res.render('admin', { title: 'The MCP | Art Forest | ATP 6 | Harvard Art Museums' });
+  res.render('admin', { title: 'The MCP | Art Forest | MAN-17| Harvard Art Museums' });
 });
 
 router.get('/test', function(req, res, next) {
-  res.render('test', { title: 'The MCP | Art Forest | ATP 6 | Harvard Art Museums' });
+  res.render('test', { title: 'Test | Art Forest | MAN-17| Harvard Art Museums' });
 });
 
 module.exports = router;
