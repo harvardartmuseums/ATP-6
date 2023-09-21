d00c853d7243d288fa590a16aab1d6faae9bfb99
Jeff Steward <steward.jeff@gmail.com>
2023-09-21 08:30:37 -0400

    add scrollin to admin page

--- interface/routes/index.js
+++ interface/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -19,11 +20,20 @@ router.get('/history', function(req, res, next) {
 });
 
 router.get('/mcp', function(req, res, next) {
-  res.render('admin', { title: 'The MCP | Art Forest | MAN-17| Harvard Art Museums' });
+  res.render('admin', {layout: 'layout-admin', title: 'The MCP | Art Forest | MAN-17| Harvard Art Museums' });
 });
 
 router.get('/test', function(req, res, next) {
   res.render('test', { title: 'Test | Art Forest | MAN-17| Harvard Art Museums' });
+});
+
+router.post('/snapshots', function(req, res, next) {
+  let base64Data = req.body.imgBase64.split(';base64,').pop();
+  let filename = `ham-forest-${(Math.floor(new Date() / 1000)).toString(36)}.png`;
+  fs.writeFile(`public\\images\\snapshots\\${filename}`, base64Data, {encoding: 'base64'}, function(err) {
+    console.log('done');
+  })
+  res.json('ok');
 });
 
 module.exports = router;
